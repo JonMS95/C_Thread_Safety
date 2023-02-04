8ded77bbc71164f255d3eb367f8e6424e770468f
JonMS95 <jon_portugalete@hotmail.com>
2023-02-04 19:49:21 +0100

    Created a new repo.

---
+++ Source_files/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <pthread.h>    // This library is mandatory when working with threads.
+
+/*
+@brief Main function. Program's entry point.
+*/
+int main(int argc, char** argv)
+{
+    return 0;
+}
