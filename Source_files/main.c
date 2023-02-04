67837edd7e0e901205dee6a0295a2895402d96a9
JonMS95 <jon_portugalete@hotmail.com>
2023-02-04 19:58:19 +0100

    Added a simple code, which calls a dummy function that counts until a huge number twice.

--- Source_files/main.c
+++ Source_files/main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <pthread.h>    // This library is mandatory when working with threads.
+#include <stdint.h>
+#include <time.h>
+
+#define BIG_NUM 1000000000UL
+uint32_t counter = 0;
+
+void count_until_BIG_NUM()
+{
+    for(int i = 0; i < BIG_NUM; i++)
+    {
+        counter++;
+    }
+}
 
 /*
 @brief Main function. Program's entry point.
 */
 int main(int argc, char** argv)
 {
+    clock_t start_time = clock();
+    // Let's say that we want to count until BIG_NUM twice:
+    count_until_BIG_NUM();
+    count_until_BIG_NUM();
+    double total_time = ((double)(clock() - start_time)) / CLOCKS_PER_SEC;
+    printf("It took the program %f seconds to execute.\r\n", total_time);
     return 0;
 }
